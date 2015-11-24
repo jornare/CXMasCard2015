7eeb847837951a6b4e7ba1105e82bc90b46c41a7
Jørn Are Hatlelid <E52760@hafslund.no>
2015-11-24 15:13:39 +0100

    signature visibility

--- js/frontScene.js
+++ js/frontScene.js
@@ -100,7 +100,7 @@ window.cx = window.cx || {};
                 }
                 this.selectedPoint && drawCross(ctx, this.selectedPoint.x, this.selectedPoint.y, 4);
             }
-            ctx.fillStyle = '#00a';
+            ctx.fillStyle = '#44a';
             ctx.font = 'italic bold 20px sans-serif';
             ctx.textBaseline = 'bottom';
             ctx.fillText(byTxt, this.width - ctx.measureText(byTxt).width - 10, this.height - 30);
